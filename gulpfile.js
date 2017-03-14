c3d0f25f2767fc653d7891a8bfc1aa0e092edd69
Derek Villars and Minh Phuong <derek+minh@>
2017-03-14 08:41:32 -0700

    add frontend bower to app

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require('gulp');
+var lib = require('bower-files')();
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
